545733c0f805f4c8072df4f1d7659d579d64b5ad
Krishna Penukonda <krishna@hypotenuse.ai>
2024-12-09 20:34:06 +0800

    :fire: Removed unused imports

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,7 +1,5 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/footer.scss"
-import { version } from "../../package.json"
-import { i18n } from "../i18n"
 
 interface Options {
   links: Record<string, string>
